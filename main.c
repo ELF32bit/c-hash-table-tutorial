f22ab81aa15bb2c525ee53f4bbf9958099cf7e3f
ELF32bit <101274435+ELF32bit@users.noreply.github.com>
2025-03-06 14:04:12 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 int main(int argc, char **argv) {
 	Set* set = set_new(4);
 
+	/* strdup() ensures that set_free() works */
+	/* elements inserted into set() must be heap allocated */
 	set_add(set, strdup("a"));
 	set_add(set, strdup("ab"));
 	set_add(set, strdup("abc"));
@@ -16,6 +18,7 @@ int main(int argc, char **argv) {
 	set_add(set, strdup("abcdefg"));
 	set_add(set, strdup("abcdefgh"));
 
+	/* inserting same element multiple times */
 	char* str1 = strdup("I");
 	set_add(set, str1);
 	set_add(set, str1);
@@ -23,11 +26,12 @@ int main(int argc, char **argv) {
 
 	set_print(set);
 
+	/* stack allocated strings are compared with heap allocated strings! */
 	if (set_find(set, "a")) { printf("%s found!\n", "a"); }
 	if (!set_find(set, "cba")) { printf("%s not found!\n", "cba"); }
-	if (set_find(set, str1)) { printf("%s found!\n", str1); }
 
 	char* str2 = strdup("II");
+	if (set_find(set, str1)) { printf("%s found!\n", str1); }
 	if (!set_find(set, str2)) { printf("%s not found!\n", str2); }
 	free(str2);
 
